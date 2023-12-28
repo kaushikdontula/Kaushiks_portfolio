2b0fc2b996568c91862ea4eec6504c4f2ad682e8
Kaushik Dontula <kaushikdontula@gmail.com>
2023-12-27 17:42:40 -0800

    changed routes without /kaushiks_portfolio

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function App() {
   return (
     <ThemeContext.Provider value={{ theme, toggleTheme }}>
       <div className={`App ${theme}`}>
-        <Router basename="/Kaushiks_portfolio">
+        {/* <Router basename="/Kaushiks_portfolio">
           <Routes>
             <Route index element={<LandingPage />} />
             <Route path="/Kaushiks_portfolio/LandingPage" element={<LandingPage />} />
@@ -102,7 +102,19 @@ function App() {
             <Route path="/Kaushiks_portfolio/Projects" element={<Projects />} />
             <Route path="/Kaushiks_portfolio/ContactMe" element={<ContactMe />} />
           </Routes>
+        </Router> */}
+
+        <Router>
+          <Routes>
+            <Route index element={<LandingPage />} />
+            <Route path="/LandingPage" element={<LandingPage />} />
+            <Route path="/Resume" element={<Resume />} />
+            <Route path="/Projects" element={<Projects />} />
+            <Route path="/ContactMe" element={<ContactMe />} />
+          </Routes>
         </Router>
+
+
       </div>
     </ThemeContext.Provider>
   );
